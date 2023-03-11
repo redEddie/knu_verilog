c39a5698682e52e6b02e3fe78678971a7f4f942a
redEddie <78861908+redEddie@users.noreply.github.com>
2023-03-11 15:09:04 +0900

    Delete ws directory

--- ws/01_buffer/run_me.bat
+++
@@ -1,8 +0,0 @@
-@REM 테스트를 어느정도 자동화해주는 파일
-@REM  윈도우라서 .bat이다. (그럼 맥은?)
-
-@REM icarus부터 , inv와 inv_tb로 .vpp파일 만드는거다
-iverilog -o inv_tb.vvp inv.v inv_tb.v 
-vvp inv_tb.vvp
-gtkwave ./inv_tb.vcd @REM gtk실행
-pause @REM 일시정지는 파형이 꺼지지마라고
--- ws/01_hello_world/run_me.bat
+++
@@ -1,5 +0,0 @@
-del tb_test_out.vcd tb_test_out.vvp
-iverilog -o tb_test_out.vvp or_gate.v or_test.v tb_testbench.v
-vvp tb_test_out.vvp
-gtkwave ./tb_test_out.vcd
-pause
