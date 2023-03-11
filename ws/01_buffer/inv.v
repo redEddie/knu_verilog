0d889eb4be49e8e9ae1274841620e0e60d4400a4
redEddie <78861908+redEddie@users.noreply.github.com>
2023-03-11 15:07:18 +0900

    Add files via upload

---
+++ ws/01_buffer/inv.v
@@ -0,0 +1,10 @@
+// inv = inverter
+
+module inv(a,b);
+
+input a;
+output b;
+
+assign b = ~a;
+
+endmodule
---
+++ ws/01_hello_world/or_gate.v
@@ -0,0 +1,10 @@
+module or_gate (in1, in2, out);
+
+input in1, in2;
+output out;
+
+assign out = in1 | in2;
+
+endmodule
+
+
