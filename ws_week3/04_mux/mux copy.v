c73f3c1207fd367352f51c3cb0e97d9b4f14b13b
redEddie <78861908+redEddie@users.noreply.github.com>
2023-03-18 14:43:09 +0900

    always에 *넣기
    *는 만능이야

--- ws_week3/04_mux/mux copy.v
+++ ws_week3/04_mux/mux copy.v
@@ -7,7 +7,7 @@ wire out;
 // 아래 코드는 algorithmic description 라고 한다.
 // sensitive 와 살짝 다르다. 코드와 닮게 조금 sequiential 하다.
 // 그래서 always 구문을 써서 sensitive하게 만들어준다.
-always @ (sel or a or b)
+always @ (*)
     if (sel)
         out <= a;
     else
