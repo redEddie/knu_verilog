ec5495a5e8bfafded93675a15d13a8579001e4fc
unknown <78861908+redEddie@users.noreply.github.com>
2023-03-25 13:40:25 +0900

    adder.v 만듬.

--- ws_week4/01_adder/adder.v
+++ ws_week4/01_adder/adder.v
@@ -6,7 +6,8 @@ module adder(
 );
 
 // wire는 정의하지 않으면 default로 wire이다.
-// wire cout;
+wire [3:0] sum;
+wire cout;
 
 // c언어는 한 번만 함수를 실행하기 때문에, 변수의 변화가 새로운 출력을 만들지 못 한다.
 // assign 은 sum과 a+b를 이어주는거
