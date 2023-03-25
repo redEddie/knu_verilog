70056be61bbd8b30e35b3d3683bc3dbf58b7d418
unknown <78861908+redEddie@users.noreply.github.com>
2023-03-25 13:54:15 +0900

    입력에 if 가능.

--- ws_week4/01_adder/tb.v
+++ ws_week4/01_adder/tb.v
@@ -1,17 +1,17 @@
 // 원래 i/o 없다.
 module tb;
 
-wire [3:0] A;
 // assign 으로 ref처럼 A를 고정할 수 도 있다.
-assign A = 4'b1111;
+// assign A = 4'b1111;
+reg [3:0] A;
+reg [3:0] B;
 
 adder dut1(
     // i/o를 지정할 때
     // input, output을 각각 지정해도 되고,
-    // .a(A) 와 같이 지정해도 되고,
-    // .a() 와 같이 지정해도 된다.
-    .a(),
-    .b(),
+    // .a(A) 와 같이 지정해도 된다.
+    .a(A),
+    .b(B),
     .cout(),
     .sum()
 );
@@ -19,21 +19,15 @@ adder dut1(
 initial begin
 
     #100
-    A = 1'b1;
-    B = 1'b0;
-    SEL = 1'b0; //select a
+    A = 4'b0001;
+    B = 4'b0010;
 
     #100
-    SEL = 1'b1; //select b
-    
-    #100
-    A = 1'b0; 
-    SEL = 1'b0;
-    
-    #20
-    A = 1'b0; //continuously affect to a node
-    
-    #100
+    // 입력 변환에 if()도 가능하다.
+    if()
+        A = 4'b0000;
+        B = 4'b0000;
+
     $finish;
 end
 
