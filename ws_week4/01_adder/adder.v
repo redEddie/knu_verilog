c7eb10c82ca92cff537d95f69b5ffb034bf8680a
unknown <78861908+redEddie@users.noreply.github.com>
2023-03-25 13:37:25 +0900

    주석추가.
    c언어와 verilog의 차이.

--- ws_week4/01_adder/adder.v
+++ ws_week4/01_adder/adder.v
@@ -5,8 +5,10 @@ module adder(
     output [3:0] sum
 );
 
-wire out;
+// wire는 정의하지 않으면 default로 wire이다.
+// wire cout;
 
-assign out = sel ? b : a;
+// c언어는 한 번만 함수를 실행하기 때문에, 변수의 변화가 새로운 출력을 만들지 못 한다.
+assign sum = a+b; // assign 은 sum과 a+b를 이어주는거
 
 endmodule
