ed58e60369375ae700b62002f1ace6a0608b7782
redEddie <78861908+redEddie@users.noreply.github.com>
2023-05-02 20:39:50 +0900

    testing

--- hw/hw_week7/parent.v
+++ hw/hw_week7/parent.v
@@ -57,27 +57,22 @@ always @(*)
 //         book <= 1'b0; // 책 그만 출력.
 
 always @(negedge resetb or posedge clk) begin
-    if(~resetb) begin
+    if(~resetb)
         state <= `P0; // 리셋당하면 initial state로 돌아감.
         food <= 1'b0; // 밥 그만 출력. 
         book <= 1'b0; // 책 그만 출력.
-    end
-    else if (state == `P0) begin
+    else if (state == `P0)
         food <= 1'b0; // 밥 그만 출력. 
         book <= 1'b0; // 책 그만 출력.
-    end
-    else if(state == `P1) begin
+    else if(state == `P1)
         food <= 1'b1; // Output food.
         book <= 1'b0; // Stop book output.
-    end
-    else if(state == `P2) begin
+    else if(state == `P2)
         food <= 1'b0; // 밥 그만 출력. 
         book <= 1'b1; // 책 출력. 
-    end
-    else begin
+    else
         food <= 1'b0; // 밥 그만 출력. 
         book <= 1'b0; // 책 그만 출력.
-    end
 end
 
 endmodule
